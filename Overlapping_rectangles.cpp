8c0bf29cb9caf7080ea656e8c4e34127408500c3
SACHIN BHOLA <43703271+Sachinbhola@users.noreply.github.com>
2022-01-04 16:34:51 +0530

    GS 2 down

--- Overlapping_rectangles.cpp
+++ Overlapping_rectangles.cpp
@@ -1,5 +1,4 @@
 int doOverlap(int L1[], int R1[], int L2[], int R2[]) {
-  
         // instead of writing many conditions to check overlap use a single condition to check not overlapping rectangles
         if(L1[0]>R2[0] || L2[0]>R1[0] || L1[1]<R2[1] || L2[1]<R1[1]) return 0;
         return 1;
