0ea4c084d46de82537b7f0007540a3656c57382b
SACHIN BHOLA <43703271+Sachinbhola@users.noreply.github.com>
2022-01-11 20:10:10 +0530

    Amazon 3 Down

---
+++ Uts and Bolts.cpp
@@ -0,0 +1,14 @@
+void matchPairs(char nuts[], char bolts[], int n) {
+	   // m['!']=0;
+	   // m['#']=1;
+	   // m['$']=2;
+	   // m['%']=3;
+	   // m['&']=4;
+	   // m['*']=5;
+	   // m['@']=6;
+	   // m['^']=7;
+	   // m['~']=8;
+	    
+	    sort(nuts,nuts+n);
+	    sort(bolts,bolts+n);
+	}
