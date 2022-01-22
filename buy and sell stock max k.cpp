3ac495931657eddd9d08de8fc952d3bd1448dca3
SACHIN BHOLA <43703271+Sachinbhola@users.noreply.github.com>
2022-01-22 12:57:21 +0530

    amazon 12 down

---
+++ buy and sell stock max k.cpp
@@ -0,0 +1,17 @@
+int maxProfit(int k, int n, int price[]) {
+        if(n<2) return 0;
+        int dp[k+1][n];
+        
+        for(int i=0;i<=k;i++) dp[i][0]=0;
+        for(int i=0;i<n;i++) dp[0][i]=0;
+        
+        for(int i=1;i<=k;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i][j-1];
+                for(int k=0;k<j;k++){
+                    dp[i][j]=max(dp[i][j],dp[i-1][k]+(price[j]-price[k]));
+                }
+            }
+        }
+        return dp[k][n-1];
+    }
